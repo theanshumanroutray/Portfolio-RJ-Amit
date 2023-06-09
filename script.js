e43fa4d18ec78067390940be22efa92ff505980d
Anshuman Routray <119794366+theanshumanroutray@users.noreply.github.com>
2023-06-09 21:19:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+let sections = document.querySelector('section');
+let navLinks = document.querySelector('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    // Sticky Navbar
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+};
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+
+});
+ScrollReveal().reveal('.homeContent', '.heading',  { origin: 'top'});
+ScrollReveal().reveal('.home-img', '.passion-container',  { origin: 'bottom'});
